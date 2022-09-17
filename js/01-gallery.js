6aa88655d71f94dc15415bd3e88bae22ee6fb1a2
Katsinskiy <katsinskiy@gmail.com>
2022-09-17 23:20:49 +0200

    hw 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+refs.gallery.addEventListener("click", onGallaryClick);
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery-item">
+  <a href="${original}" class = "gallery__link">
+    <img class ="gallery__image" data-source = '${original}' src="${preview}" alt="${description}">
+  </a>
+</div>`;
+  })
+  .join("");
+
+refs.gallery.insertAdjacentHTML("beforeend", markup);
+
+const instance = basicLightbox.create(`<img src=''/>`, {
+  onShow: () => window.addEventListener("keydown", onEscapePress),
+  onClose: () => window.removeEventListener("keydown", onEscapePress),
+});
+
+function onGallaryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance.element().querySelector("img").src = event.target.dataset.source;
+  instance.show();
+}
+
+function onEscapePress(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+}
+
+
